7d48576c356973755c1d357a3698cea8a9eaa5e1
Lin <108180851+L1nMay@users.noreply.github.com>
2025-04-23 20:09:39 +0300

    Delete lab04dz/hello_world directory

--- lab04dz/hello_world/include/my_header.h
+++
@@ -1,8 +0,0 @@
-#ifndef MY_HEADER_H
-#define MY_HEADER_H
-
-int get_value() {
-    return 42; // Example value
-}
-
-#endif
--- lab04dz/hello_world/src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "my_header.h"
-
-int main() {
-    std::cout << "Hello, World! Value from header: " << get_value() << std::endl;
-    return 0;
-}
