0fd8d340ad5b9800c7201a5fd0ab7066276fcf1e
L1nMay <ilazayzev@gmail.com>
2025-04-23 06:28:46 -0400

    Add project

---
+++ lab04dz/hello_world/include/my_header.h
@@ -0,0 +1,8 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+
+int get_value() {
+    return 42; // Example value
+}
+
+#endif
---
+++ lab04dz/hello_world/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "my_header.h"
+
+int main() {
+    std::cout << "Hello, World! Value from header: " << get_value() << std::endl;
+    return 0;
+}
