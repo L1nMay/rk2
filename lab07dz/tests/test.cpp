5247439a89095a6ee963d62b1e22f7f389470dca
L1nMay <ilazayzev@gmail.com>
2025-05-04 04:25:13 -0400

    Fix CMake and GitHub Actions

--- lab07dz/tests/test.cpp
+++ lab07dz/tests/test.cpp
@@ -2,7 +2,7 @@
 #include <cassert>
 
 int main() {
-    assert(lab07dz::calculate(3, 4) == 12);
-    lab07dz::show_result("multiplication");
+    assert(lab07dz::calculate(2, 3) == 6);
+    lab07dz::show_result("2 * 3");
     return 0;
 }
