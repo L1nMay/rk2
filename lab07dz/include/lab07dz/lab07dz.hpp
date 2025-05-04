43b02723538cad1f5573a53b5896394e174a579a
Lin <108180851+L1nMay@users.noreply.github.com>
2025-05-04 11:32:52 +0300

    Delete lab07dz directory

--- lab07dz/include/lab07dz/lab07dz.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace lab07dz {
-    int calculate(int a, int b);
-    void show_result(const char* operation);
-}
--- lab07dz/tests/test.cpp
+++
@@ -1,8 +0,0 @@
-#include "lab07dz/lab07dz.hpp"
-#include <cassert>
-
-int main() {
-    assert(lab07dz::calculate(2, 3) == 6);
-    lab07dz::show_result("2 * 3");
-    return 0;
-}
